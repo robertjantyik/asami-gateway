7a476ea19aba18ce425d7ff9aaf8b2670b9ff31a
robert.jantyik <robert.jantyik@dextraline.hu>
2022-08-31 10:37:54 +0200

    init

---
+++ src/test/java/hu/asami/asamigateway/AsamiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.asami.asamigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsamiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
